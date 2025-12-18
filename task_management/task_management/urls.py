27a1da5bb7dda0d7e8f86007bc909bab24d8f1c8
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-18 16:28:39 +0300

    fixing bugs

--- task_management/task_management/urls.py
+++ task_management/task_management/urls.py
@@ -2,7 +2,7 @@
 URL configuration for task_management project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
